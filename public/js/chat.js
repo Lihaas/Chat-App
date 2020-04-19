659a752bb3bc8020aeef4a39e7be93cd824c2d3a
Sahil Verma <vermasahilcf5@gmail.com>
2020-04-19 18:03:26 +0530

    remove side bar

--- public/js/chat.js
+++ public/js/chat.js
@@ -12,6 +12,18 @@ const messageTemplate = document.querySelector('#message-template').innerHTML
 const locationMessageTemplate = document.querySelector('#location-message-template').innerHTML
 const sidebarTemplate = document.querySelector('#sidebar-template').innerHTML
 
+/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+  }
+  
+  /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+  }
+
 // Options
 const { username, room } = Qs.parse(location.search, { ignoreQueryPrefix: true })
 
